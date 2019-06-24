f852f8799a46af2bcbc59c864d0d23b917a716a6
欧建明 <oujianming@tapmega.com>
2019-06-24 16:15:45 +0800

    Add comments

--- MojApi/src/main/java/com/moj/mojapi/MojKnife.java
+++ MojApi/src/main/java/com/moj/mojapi/MojKnife.java
@@ -3,12 +3,13 @@ package com.moj.mojapi;
 /**
  * @author : moj
  * @date : 2019/6/24
- * @description :
+ * @description : 面向开发者的接入类
  */
 public class MojKnife {
 
     public static void bind(Object obj){
         try{
+            //目前只支持传入activity或View类型等有findViewById方法的对象
             String s = obj.getClass().getName() +"_ViewBinding";
             ViewBinding binding = (ViewBinding) obj.getClass().getClassLoader().loadClass(s).newInstance();
             binding.bindView(obj);
